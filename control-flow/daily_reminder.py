45dabaad69a0324bb4c996e2133e4d48236feffb
LaurentiaBops <bopela.nb@gmail.com>
2025-07-20 23:10:51 +0200

    Debugging reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -7,16 +7,16 @@ priority_variable = input("Priority (high/medium/low): ") #Task's priority
 time_bound = input("Is it time-bound? (yes/no): ")
 
 #Define a function that defines reminder message based on user inputs
-def reminder(priority,time):
+def reminder(priority,time_bound):
     match priority:
         case "high":
-            if time == "yes":
+            if time_bound == "yes":
                 print(f"Reminder: '{task_variable}' is a {priority} priority task that requires immediate attention today!")
-            elif time == "no":
+            elif time_bound == "no":
                 print(f"Note: '{task_variable}' is a {priority} priority task. Consider completing it when you have free time.")
         case "medium":
-            if time == "yes":
+            if time_bound == "yes":
                 print(f"Reminder: '{task_variable}' is a {priority} priority task that requires immediate attention today!")
-            elif time == "no":
+            elif time_bound == "no":
                 print(f"Note: '{task_variable}' is a {priority} priority task. Consider completing it when you have free time.")
 reminder(priority_variable,time_bound)

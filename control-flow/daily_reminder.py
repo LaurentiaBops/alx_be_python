883fa398eab78f5d0a6b46679ab1084f97bf912e
LaurentiaBops <bopela.nb@gmail.com>
2025-07-20 23:00:21 +0200

    Use conditional statements, match case, and loops to remind user about task

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,18 @@
+#Use conditional statements, Match Case, and loops to remind
+#user about a single, priority task for the day based on time sensitivity
+
+#Prompt for a single task
+task_variable = input("Enter your task: ") #Enter task description
+priority_variable = input("Priority (high/medium/low): ") #Task's priority
+time_bound = input("Is it time-bound? (yes/no): ")
+
+#Define a function that defines reminder message based on user inputs
+def reminder(priority,time):
+    match priority:
+        case "high" | "medium" | "low":
+            if time == "yes":
+                print(f"Reminder: '{task_variable}' is a {priority} priority task that requires immediate attention today!")
+            elif time == "no":
+                print(f"Note: '{task_variable}' is a {priority} priority task. Consider completing it when you have free time.")
+
+reminder(priority_variable,time_bound)

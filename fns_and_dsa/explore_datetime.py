d316a4a1ce0f8c4d8c0619d62f9a1c78ae75c33f
LaurentiaBops <bopela.nb@gmail.com>
2025-08-02 12:19:10 +0200

    Improve syntax of future date in explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -15,7 +15,7 @@ def calculate_future_date ():
     number_of_days = int(input("Enter the number of days to add to the current date: "))
     now = datetime.now()              
     future_date = now.date() + timedelta(days = number_of_days)
-    print("Future date: ",future_date)
+    print("Future date: ",future_date.strftime("%Y-%m-%d"))
 
 calculate_future_date()
 

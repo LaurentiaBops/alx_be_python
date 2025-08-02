63976ee9448b7a092182d3e0b2ef88d8b30c3c56
LaurentiaBops <bopela.nb@gmail.com>
2025-08-02 11:37:19 +0200

    File to perform specified operations with dates and times
    Python script named explore_datetime.py uses datetime module
    to obtain the current date and time and calculate a future day

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,23 @@
+# Performs specified operations with dates and times
+
+import datetime
+from datetime import datetime, timedelta
+
+#Determine the current date and time
+def display_current_datetime ():
+   now = datetime.now()
+   print("Current Date and Time:",now) 
+
+display_current_datetime()
+
+#Calculate a future day
+def calculate_future_date ():
+    number_of_days = int(input("Enter the number of days to add to the current date: "))
+    now = datetime.now()              
+    future_date = now.date() + timedelta(days = number_of_days)
+    print("Future date: ",future_date)
+
+calculate_future_date()
+
+if __name__ == "__main__":
+    main()

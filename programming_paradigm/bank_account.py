558a291f223c66c77be1a8a5fd9955697d10e70d
LaurentiaBops <bopela.nb@gmail.com>
2025-08-03 20:37:45 +0200

    Create a class that encapsulates simple banking operations

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+class BankAccount:
+    def __init__(self,account_balance=0):
+        self.account_balance = account_balance
+    
+    def deposit(self,amount):
+        current_balance = amount + self.account_balance
+        return current_balance
+
+    def withdraw(self,amount):
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+
+        print(f"Current Balance: ${self.account_balance}")
+
+
+
+

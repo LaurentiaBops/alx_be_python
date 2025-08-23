f9d0e8b3def4cc90331718f8a3e867443c4e4f66
LaurentiaBops <bopela.nb@gmail.com>
2025-08-23 06:21:52 +0200

    Implementing a system that tracks books in a library
    Use script main.py for testing by importing
    the book and library class in library_management.py

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,70 @@
+# System that tracks books in a library
+
+class Book:
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def is_checked_out(self):
+        """Keeps track of the avaliabilty of books"""
+        return self._is_checked_out
+
+    def __str__(self):
+        """Convert object to a string"""
+        return f"{self.title} by {self.author}"
+
+
+
+class Library(Book):
+    def __init__(self):
+        self._books = []
+        self._checked_out_books = []
+    
+    
+    def add_book(self,book):
+        """Adds new books to the library system"""
+        self._books.append(book)
+    
+    def check_out_book(self,title):
+        """Checks out a book and marks it as unavaliable"""
+        for book in self._books:
+            if book.title == title:
+                if book.is_checked_out:
+                    book._is_checked_out = True
+                    self._books.remove(book)
+                    self._checked_out_books.append(book)
+                    return
+                else:
+                    return
+        
+        
+
+    
+    def return_book(self,title):
+        """Returning a book to the library system and making it avaliable again"""
+        for book in self._checked_out_books:
+            if book.title == title:
+                book._is_checked_out = False
+                self._books.append(book)
+                self._checked_out_books.remove(book)
+                return
+                
+            
+            else:
+                return
+
+
+         
+
+    def list_available_books(self):
+        """Lists all avaliable books"""
+        for book in self._books:
+            print(f"-{book}")
+
+
+
+
+     
+        
+
